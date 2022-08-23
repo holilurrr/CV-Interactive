4db156310634bbbc96bd37b0cd0b8e51328b7e03
Holilur Rahman <holilurrhmn9@gmail.com>
2022-08-23 15:31:53 +0700

    ganti profile

--- src/views/SkillView.vue
+++ src/views/SkillView.vue
@@ -53,6 +53,9 @@
     </div>
     </div>
   </main>
+    <section class="ds-info-list-section">
+      <h3 class="">Latest Works</h3>
+    </section>
   <!-- <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
